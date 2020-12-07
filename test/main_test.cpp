14e69b8d70b81b5f1fa171d08a93a9cd8c4c61fd
Andrei <grigormaklaud@gmai.com>
2020-12-07 13:44:44 +0300

    Revert "remove all files"
    This reverts commit 461e7861b5527fb01ca892552a7608b1b17fe9ee.

---
+++ test/main_test.cpp
@@ -0,0 +1,51 @@
+#include <list>
+#include <gtest/gtest.h>
+#include "allocator/otusAllocator.hpp"
+#include "sList/simpleList.hpp"
+
+using namespace std;
+using namespace slist;
+using namespace simple_allocator;
+
+TEST(allocator, overflow)
+{
+  std::list<double, Light_Pool_Allocator<double, 216>> list;
+  ASSERT_NO_THROW(list.push_back(0));
+  ASSERT_NO_THROW(list.push_back(0));
+}
+
+TEST(allocator, equal)
+{
+  std::list<double, Light_Pool_Allocator<double, 216>> list1;
+  ASSERT_NO_THROW(list1.push_back(0));
+  ASSERT_NO_THROW(list1.push_back(0));
+
+  std::list<double, Light_Pool_Allocator<double, 216>> list2(list1);
+
+  ASSERT_EQ(list1, list2);
+}
+
+TEST(allocator, compare)
+{
+
+  list<double, Light_Pool_Allocator<double, 24>> list1;
+  list<double, Light_Pool_Allocator<double, 24>> list2;
+
+  ASSERT_NE(list1.get_allocator(), list2.get_allocator());
+}
+
+TEST(allocator, alloc_dealloc)
+{
+  Light_Pool_Allocator<double, 64> allocator;
+  double* ptr1 = allocator.allocate(1);
+  allocator.deallocate(ptr1);
+  double* ptr2 = allocator.allocate(1);
+
+  ASSERT_EQ(ptr1,ptr2);
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
